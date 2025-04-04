4fae82e6a4df83cad682a90d8fc15048027bc0b8
Marina S. Diniz <97061354+MarinaSDiniz@users.noreply.github.com>
2025-04-04 16:43:26 -0300

    TabView
    Home, tabview

--- Projeto/PROJETO/PROJETO/ContentView.swift
+++ Projeto/PROJETO/PROJETO/ContentView.swift
@@ -8,11 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var selectedIndex = 0
+    @State var presented = false
+    
+    let icons = [
+        "house",
+        "medal",
+        "plus",
+        "cube.box",
+        "gear"
+        ]
     var body: some View {
-            TabView{
-                
+        VStack{
+            ZStack{
+                Spacer().fullScreenCover(isPresented: $presented, content: {
+                    Button(action: {
+                        presented.toggle()
+                    }, label: {
+                        Text("Close")
+                            .frame(width: 200, height: 50)
+                            .background(Color.pink)
+                            .cornerRadius(12)
+                    })
+                })
+                switch selectedIndex {
+                case 0:
+                    NavigationView {
+                        VStack{
+                            HOME()
+                        }
+                        .navigationTitle("Home")
+                    }
+                case 1:
+                    NavigationView {
+                        VStack{
+                            Text("segunda")
+                        }
+                        .navigationTitle("Conquistas")
+                    }
+                case 3:
+                    NavigationView {
+                        VStack{
+                            Text("terceira")
+                        }
+                        .navigationTitle("Criar")
+                    }
+                case 4:
+                    NavigationView {
+                        VStack{
+                            Text("quarta")
+                        }
+                        .navigationTitle("Tarefas")
+                    }
+                default:
+                    NavigationView {
+                        VStack{
+                            Text("config")
+                        }
+                        .navigationTitle("Configuracoes")
+                    }
+                }
             }
-        .padding()
+           
+            
+            //Divider()
+            HStack{
+                ForEach(0..<5, id: \.self){
+                    number in
+                    Spacer()
+                    Button(action: {
+                        if number == 2 {
+                            presented.toggle()
+                        } else {
+                            self.selectedIndex = number
+                        }
+                    }, label: {
+                        if number == 2 {
+                            Image(systemName: icons[number])
+                                .font(.system(size: 25, weight: .regular, design: .default))
+                                .foregroundColor(.white)
+                                .frame(width: 60, height: 60)
+                                .background(Color.purple)
+                                .cornerRadius(50)
+                                .offset(x: 0, y: -10)
+                        } else {
+                            Image(systemName: icons[number])
+                                .font(.system(size: 25, weight: .regular, design: .default))
+                                .foregroundColor(selectedIndex == number ? .black : Color(UIColor.lightGray))
+                        }
+                    })
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
--- Projeto/PROJETO/PROJETO/HOME.swift
+++ Projeto/PROJETO/PROJETO/HOME.swift
@@ -2,39 +2,107 @@ import SwiftUI
 
 struct HOME: View {
     var body: some View {
-        ZStack{
-            VStack{
+        ScrollView(.horizontal){
+            
+            ZStack{
+                Color.black
+                HStack{
+                    // primeiro quadrado
+                    ZStack{
+                        Color.yelow
+                            .ignoresSafeArea()
+                            .frame(width: 390, height: .infinity)
+                            
+                        
+                        VStack{
+                            //imagem do topo
+                                Image(systemName: "sun.max.fill")
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                            .padding(.init(top: -1, leading: -120, bottom: 0, trailing: 0))
                 
-                //imagens dentro do retangulo
-                
-                RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
-                    .frame(width: 300, height: 170)
-                    .foregroundStyle(.yelow)
-                    .padding()
-                
-                //tarefas dentro dos retangulos
-                
-                //imagens dentro do retangulo
-                
-                RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
-                    .frame(width: 300, height: 170)
-                    .foregroundStyle(.lightblue)
-                    .padding()
-                
-                //tarefas dentro dos retangulos
-
-                //imagens dentro do retangulo
-                
-                RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
-                    .frame(width: 300, height: 170)
-                    .foregroundStyle(.darkblue)
-                    .padding()
-                
-                //tarefas dentro dos retangulos
-                
+                            //tarefas
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                                .frame(width: 250, height: 120)
+                                .foregroundStyle(.lightyellow)
+                            
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                                .frame(width: 250, height: 120)
+                                .foregroundStyle(.lightyellow)
+                            
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                                .frame(width: 250, height: 120)
+                                .foregroundStyle(.lightyellow)
+                        }
+                    }
+                    //segundo quadrado
+                    
+                    ZStack{
+                        
+                        Color.lightblue
+                            .ignoresSafeArea()
+                            .frame(width: 390, height: .infinity)
+                        VStack{
+                            //imagem
+                            
+                        
+                                Image(systemName: "cloud.sun.fill")
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                            .padding(.init(top: -110, leading: -120, bottom: 0, trailing: 0))
+                            
+                            
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                                .frame(width: 250, height: 120)
+                                .foregroundStyle(.mediumblue)
+                            
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                                .frame(width: 250, height: 120)
+                                .foregroundStyle(.mediumblue)
+                            
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                                .frame(width: 250, height: 120)
+                                .foregroundStyle(.mediumblue)
+                        }
+                    }
+                    //terceiro quadrado
+                    
+                    ZStack{
+                        Color.darkblue
+                            .ignoresSafeArea()
+                            .frame(width: 390, height: .infinity)
+                        
+                        VStack{
+                            //imagem topo
+                                
+                                    Image(systemName: "moon.stars.fill")
+                                    .foregroundColor(.white)
+                                    .font(.largeTitle)
+                                .padding(.init(top: -110, leading: -120, bottom: 0, trailing: 0))
+                                
+                            
+                            //
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                                .frame(width: 250, height: 120)
+                                .foregroundStyle(.babyblue)
+                            
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                                .frame(width: 250, height: 120)
+                                .foregroundStyle(.babyblue)
+                            
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                                .frame(width: 250, height: 120)
+                                .foregroundStyle(.babyblue)
+                        }
+                    }
+                    
+                }
+                .ignoresSafeArea()
             }
-            Spacer()
+        
         }
+        .scrollTargetBehavior(.viewAligned)
+        .ignoresSafeArea()
     }
 }
 #Preview {
